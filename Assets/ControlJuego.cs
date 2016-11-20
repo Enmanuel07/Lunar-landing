b44a6c60cb78d9d6a93f41b687377774ec96ab70
Enmanuel <enmanuel.bueno@gmail.com>
2016-11-20 18:41:57 -0400

    Juego iniciado. Agregado propulsor espacial (nave), movimiento de la nave y sonido al momento de activar los propulsores

---
+++ Assets/ControlJuego.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlJuego : MonoBehaviour
+{
+    public Transform ubicacionNave;
+
+    private float velocidadCam = 1.2f;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovimientoCamara();
+    }
+    void MovimientoCamara()
+    {
+        if (ubicacionNave != null)
+        {
+            Vector2 nuevaPosicion = Vector2.Lerp(transform.position, ubicacionNave.position, Time.deltaTime * velocidadCam);
+            Vector3 posicionCamara = new Vector3(nuevaPosicion.x, nuevaPosicion.y, -10f);
+            transform.position = new Vector3(posicionCamara.x, posicionCamara.y, -10f);
+        }
+    }
+}
---
+++ Assets/Propulsor espacial/ControlNave.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlNave : MonoBehaviour {
+
+    // Use this for initialization
+    public ParticleEmitter propulsorCentro;
+    public ParticleEmitter propulsorIzquierdo;
+    public ParticleEmitter propulsorDerecho;
+    public AudioSource sonidoPropulsor;
+    private bool reproducirPropulsor;
+    private float gravedad = 1.62519f;
+    private float fuerzaPropulsor = 3f;
+    private float combustible = 100;
+
+	void Start () {
+        reproducirPropulsor = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movimientoNave();
+        if (reproducirPropulsor)
+        {
+            ReproducirPropulsor();
+        }
+        else
+            sonidoPropulsor.Pause();
+    }
+    void movimientoNave()
+    {
+        Quaternion rotationVector = GetComponent<Rigidbody>().rotation;
+        if (combustible > 0)
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                GetComponent<Rigidbody>().AddForce(transform.right * fuerzaPropulsor);
+                rotationVector.z -= Input.GetAxis("Horizontal") * 0.1f * Time.deltaTime;
+                propulsorIzquierdo.emit = true;
+                propulsorDerecho.emit = false;
+                combustible -= 10 * Time.deltaTime;
+                reproducirPropulsor = true;
+            }
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                GetComponent<Rigidbody>().AddForce(transform.right * -fuerzaPropulsor);
+                rotationVector.z -= Input.GetAxis("Horizontal") * 0.1f * Time.deltaTime;
+                propulsorIzquierdo.emit = false;
+                propulsorDerecho.emit = true;
+                combustible -= 10 * Time.deltaTime;
+                reproducirPropulsor = true;
+            }
+            if (Input.GetAxis("Horizontal") == 0)
+            {
+                propulsorIzquierdo.emit = false;
+                propulsorDerecho.emit = false;
+                reproducirPropulsor = false;
+            }
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                GetComponent<Rigidbody>().AddForce(transform.up * fuerzaPropulsor);
+                propulsorCentro.emit = true;
+                combustible -= 20 * Time.deltaTime;
+                reproducirPropulsor = true;
+            }
+            if (Input.GetAxis("Vertical") == 0)
+            {
+                propulsorCentro.emit = false;
+            }
+            GetComponent<Rigidbody>().MoveRotation(rotationVector);
+        }
+        else
+        {
+            reproducirPropulsor = false;
+            propulsorCentro.emit = false;
+            propulsorDerecho.emit = false;
+            propulsorIzquierdo.emit = false;
+        }
+        GetComponent<Rigidbody>().AddForce(transform.up * -gravedad);
+    }
+    private void ReproducirPropulsor()
+    {
+        if (sonidoPropulsor.isPlaying)
+        {
+            return;
+        }
+        sonidoPropulsor.Play();
+    }
+}
