ad19a6b8a6046bbdb42c7167414c62b9247a8795
Candido Pichardo <gregorip@gmail.com>
2016-11-23 05:08:32 -0400

    Crear bloque powerup, poder de escudo, prefabs de gasolina y plataforma y programacion inicial de movimiento circular uniforme para el escudo

---
+++ Assets/Scripts/ShieldController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Utilities;
+
+public class ShieldController : MonoBehaviour {
+
+    UniformCircularMotionBehaviour uniformCircularMotionBehaviour;
+    private float _speed;
+    private float _acceleration;
+    private float _radius;
+
+    // Use this for initialization
+    void Start () {
+
+        _speed = 5.0f;
+        _acceleration = 1.0f;
+        _radius = 1.0f;
+
+        uniformCircularMotionBehaviour = new UniformCircularMotionBehaviour(_speed, _acceleration, _radius);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Move();
+    }
+
+    void Move() {
+
+        transform.localPosition = uniformCircularMotionBehaviour.GeneratePosition();
+
+    }
+}
---
+++ Assets/Utilities/UniformCircularMotion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Utilities {
+    class UniformCircularMotionBehaviour {
+
+        private float _angle;
+        private float _time;
+
+        public float Speed { get; set; }
+        public float Acceleration { get; set; }
+        public float Radius { get; set; }
+
+        public UniformCircularMotionBehaviour() {
+
+        }
+
+        public UniformCircularMotionBehaviour(float speed, float acceleration, float radius) {
+
+            Speed = speed;
+            Acceleration = acceleration;
+            Radius = radius;
+        }
+
+        public Vector3 GeneratePosition() {
+
+            _time += UnityEngine.Time.deltaTime;
+
+            Speed += Acceleration * (_time);
+            _angle = Speed * _time / Radius;
+
+            return new Vector3(Radius * Mathf.Cos(_angle), Radius * Mathf.Sin(_angle));
+
+        }
+
+    }
+}
