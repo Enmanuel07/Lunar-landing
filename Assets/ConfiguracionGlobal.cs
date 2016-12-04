cc639413e37793d9541bb00ce6a18d4485350f59
Enmanuel <enmanuel.bueno@gmail.com>
2016-12-04 02:24:55 -0400

    Modificado menu, opcion de deshabilitar sonidos o efectos agregada

---
+++ Assets/ConfiguracionGlobal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConfiguracionGlobal : MonoBehaviour {
+
+    // Use this for initialization
+    [HideInInspector]static public bool musica;
+    [HideInInspector]static public bool efecto;
+	void Start () {
+        musica = true;
+        efecto = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void setMusica(bool valor)
+    {
+        musica = valor;
+    }
+    public void setEfecto(bool valor)
+    {
+        efecto = valor;
+    }
+}
